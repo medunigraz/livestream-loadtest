4ce95393f6e47234ea51c2417824bb897db3c008
Michael Fladischer <FladischerMichael@fladi.at>
2021-09-23 10:44:37 +0200

    Reformat with black.

--- amazon_aws.py
+++ amazon_aws.py
@@ -7,19 +7,19 @@ from tqdm import tqdm
 
 running = True
 
-@click.command()
-@click.option('--amount', default=1, help="NONE")
-@click.option('--time', default=30, help="NONE")
-@click.option('--file', help="NONE")
 
+@click.command()
+@click.option("--amount", default=1, help="NONE")
+@click.option("--time", default=30, help="NONE")
+@click.option("--file", help="NONE")
 def start(amount, time):
 
-  #signal.signal(signal.SIGTERM, handler)
-  #signal.signal(signal.SIGHUP, handler)
+    # signal.signal(signal.SIGTERM, handler)
+    # signal.signal(signal.SIGHUP, handler)
 
-  instances = []
+    instances = []
 
-  """
+    """
   while running:
     instances.append(instance(amount))
 
@@ -32,103 +32,106 @@ def start(amount, time):
     click.echo(f"Instance is {ec2.state['Name']}.")
   """
 
-  try:
+    try:
 
-    while running:
-      new = list(instance(amount))
-      instances.extend(new)
-      #[wait(ec2) for ec2 in new]
+        while running:
+            new = list(instance(amount))
+            instances.extend(new)
+            # [wait(ec2) for ec2 in new]
 
-      for ec2 in tqdm(new):
-        wait(ec2)
+            for ec2 in tqdm(new):
+                wait(ec2)
 
-      random_time = random.randint(1, time)
-      sleep(random_time)
-    
-  except KeyboardInterrupt:
-    pass
+            random_time = random.randint(1, time)
+            sleep(random_time)
 
-  for ec2 in tqdm(instances):
+    except KeyboardInterrupt:
+        pass
 
-    click.echo(f"Waiting for instance to be up - {ec2.instance_id}... ")
-    ec2.wait_until_running()
-    click.echo(f"Terminating instance - {ec2.instance_id}... ")
-    ec2.terminate()
+    for ec2 in tqdm(instances):
 
-  for ec2 in tqdm(instances):
-    
-    click.echo(f"Waiting for instance to be terminated - {ec2.instance_id}... ")
-    ec2.wait_until_terminated()
-    click.echo(f"Instance {ec2.instance_id} is {ec2.state['Name']}.")
+        click.echo(f"Waiting for instance to be up - {ec2.instance_id}... ")
+        ec2.wait_until_running()
+        click.echo(f"Terminating instance - {ec2.instance_id}... ")
+        ec2.terminate()
+
+    for ec2 in tqdm(instances):
+
+        click.echo(f"Waiting for instance to be terminated - {ec2.instance_id}... ")
+        ec2.wait_until_terminated()
+        click.echo(f"Instance {ec2.instance_id} is {ec2.state['Name']}.")
 
 
-#def handler(signum, frame):
+# def handler(signum, frame):
 #  running = False
+
 
 def instance(amount):
 
-  number = random.randint(1, amount)
+    number = random.randint(1, amount)
 
-  click.echo(f"Starting {number} Instances.")
+    click.echo(f"Starting {number} Instances.")
 
-  for instance in range(number):
+    for instance in range(number):
 
-    ec2_client = boto3.client('ec2')
-    ec2_image = boto3.resource('ec2')
+        ec2_client = boto3.client("ec2")
+        ec2_image = boto3.resource("ec2")
 
-    images = ec2_client.describe_images(
-      Filters=[
-            {
-                'Name': 'name',
-                'Values': [
-                    'amzn2-ami-hvm*',
-                ]
-            },
-            {
-                'Name': 'owner-alias',
-                'Values': [
-                    'amazon',
-                ]
-            },
-        ],
-    )
+        images = ec2_client.describe_images(
+            Filters=[
+                {
+                    "Name": "name",
+                    "Values": [
+                        "amzn2-ami-hvm*",
+                    ],
+                },
+                {
+                    "Name": "owner-alias",
+                    "Values": [
+                        "amazon",
+                    ],
+                },
+            ],
+        )
 
-    AMI = ec2_image.Image(images['Images'][0]['ImageId'])
+        AMI = ec2_image.Image(images["Images"][0]["ImageId"])
 
-    if AMI.state == "available":
+        if AMI.state == "available":
 
-      click.echo("ImageID: " + str(AMI.image_id))
+            click.echo("ImageID: " + str(AMI.image_id))
 
-      instance = ec2_client.run_instances(
-        ImageId=AMI.image_id,
-        InstanceType='t2.nano',
-        MaxCount=1,
-        MinCount=1,
-        DryRun=False,
-        #Ipv6AddressCount=1
-        user_data=user_data_script
-      )
+            instance = ec2_client.run_instances(
+                ImageId=AMI.image_id,
+                InstanceType="t2.nano",
+                MaxCount=1,
+                MinCount=1,
+                DryRun=False,
+                # Ipv6AddressCount=1
+                user_data=user_data_script,
+            )
 
-      ec2_instance = boto3.resource('ec2')
-      ec2 = ec2_instance.Instance(instance['Instances'][0]['InstanceId'])
+            ec2_instance = boto3.resource("ec2")
+            ec2 = ec2_instance.Instance(instance["Instances"][0]["InstanceId"])
 
-      click.echo(f"Starting Instance: {(ec2.instance_id)}...")
- 
-      yield ec2
+            click.echo(f"Starting Instance: {(ec2.instance_id)}...")
 
-      #ec2.terminate()
-      #ec2.wait_until_terminated()
-      #click.echo(f"Instance is {ec2.state['Name']}.")
+            yield ec2
 
-    else: 
-      click.echo("The AMI was not available!")
+            # ec2.terminate()
+            # ec2.wait_until_terminated()
+            # click.echo(f"Instance is {ec2.state['Name']}.")
+
+        else:
+            click.echo("The AMI was not available!")
+
 
 def wait(ec2):
-  click.echo(f"Waiting for Instance {ec2.instance_id}.")
-  ec2.wait_until_running()
-  click.echo(f"Instance {ec2.instance_id} is {ec2.state['Name']}.")
+    click.echo(f"Waiting for Instance {ec2.instance_id}.")
+    ec2.wait_until_running()
+    click.echo(f"Instance {ec2.instance_id} is {ec2.state['Name']}.")
 
-  return ec2
+    return ec2
 
-if __name__ == '__main__':
-  start()
+
+if __name__ == "__main__":
+    start()
--- hlsloadclient.py
+++ hlsloadclient.py
@@ -1,38 +1,55 @@
+import sys
 import click
 import streamlink
 import requests
+from requests.auth import HTTPBasicAuth
 from datetime import datetime
 from pebble import ProcessPool, ThreadPool
+from functools import partial
 
 
 @click.command()
-@click.option('--event', help='URL to event viewer dispatcher.')
-@click.option('--viewers', default=1, help='The number of parallel viewers to start.')
-def main(event, viewers):
+@click.option("--event", help="URL to event viewer dispatcher.")
+@click.option("--viewers", default=1, help="The number of parallel viewers to start.")
+@click.option("--username", help="The username for viewer creation.")
+@click.option("--password", help="The password for viewer creation.")
+def main(event, viewers, username, password):
+    if username and password:
+        auth = HTTPBasicAuth(username, password)
+    else:
+        auth = None
     with ProcessPool(max_workers=viewers) as pp:
-        return [p.result() for p in pp.map(viewer, [event] * viewers, chunksize=1)]
+        return [
+            p.result()
+            for p in pp.map(
+                partial(viewer, auth=auth), [event] * viewers, chunksize=1
+            ).result()
+        ]
 
 
-def viewer(event):
+def viewer(event, auth=None):
     try:
-        resp = requests.put(event)
+        resp = requests.post(event, auth=auth)
         resp.raise_for_status()
     except requests.HTTPError as e:
-        # TODO: Bail out because we could no create a viewer.
+        print(e)
         return
-    url = resp.json().get("url")
-    streams = streamlink.streams(url)
+    urls = resp.json().get("streams").values()
 
-    with ThreadPool(max_workers=len(streams)) as tp:
-        return [t.result() for t in tp.map(consume, streams, chunk_size=1)]
+    with ThreadPool(max_workers=len(urls)) as tp:
+        return [t.result() for t in tp.map(consume, urls, chunk_size=1).result()]
 
 
-def consume(stream, chunk_size=4096):
+def consume(url):
+    print(f"Consuming {url}")
+    stream = streamlink.streams(url).get("best")
     start = datetime.now()
     size = 0
     with stream.open() as fd:
-        while chunk := fd.read(chunk_size):
+        while chunk := fd.read(4194304):
             size = size + len(chunk)
+            print(".", end="")
+            sys.stdout.flush()
     return datetime.now() - start, size
 
 
